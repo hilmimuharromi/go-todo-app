289cb50f5f0a589023ed98fdf5d178e9d0683f94
Hilmi Muharromi <57851583+hilmimuharromi@users.noreply.github.com>
2024-07-07 00:55:09 +0700

    Update route.go

--- routers/route.go
+++ routers/route.go
@@ -8,7 +8,7 @@ import (
 func SetupRouters() {
 	app := fiber.New()
 	app.Get("/ping", func(c *fiber.Ctx) error {
-		c.JSON(fiber.Map{"data": "pong test bro"})
+		c.JSON(fiber.Map{"data": "pong test bro test"})
 		return nil
 	})
 
